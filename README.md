5415dc4352d88ae43d428d63bd00a9da8fb38afa
Kenneth Lin <kenworthe@gmail.com>
2016-11-18 00:38:23 -0500

    edited physics values, adjusted bounce, fixed wall bug

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
+# Ping vs. Pong
+***********************
+### By: Kenneth Lin
+***********************
+
+## Overview: 
+	I based my project on the classic arcade game Pong. In both sports and competitive gaming, balance is crucial in order for competitors to demonstrate skill - and to me, Pong represents the earliest iteration of that idea. I used HTML canvas and Javascript to handle all game engine, and 
+
+	Check it out here! 
+	(URL)
+
+## Technologies Used: 
+
+* HTML, CSS, Javascript, jQuery
+
+**Website Hosting:** 
+* Firebase
+
+**Other:** 
+* Git/GitHub
+
+
+
 -Explanation of why I chose Performance.now();
-https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
+https://developer.mozilla.org/en-US/docs/Web/API/Performance/
+
 -paddle UP/DOWN movement was easy
 -how am I going to do paddle LEFT/RIGHT?
 -ball wall bounce logic was easy
 -collision detection: "too precise". try using hitbox or ray cast
--
+-added curve by changing the speed of y (rate of change).
+-curves mimick forehand swings by player.
+
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Project 1
--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var app = {};
+let app = {};
 
 function startApp(){
 	app.canvas = document.getElementById('myCanvas');
@@ -11,6 +11,11 @@ function startApp(){
 
 	restartGame();
 }
+
+//function new game 
+//draw Start
+
+
 
 function restartGame(){
 //reset score = 0;
@@ -26,9 +31,8 @@ function frameUpdate(timeStamp){
 	window.requestAnimationFrame(frameUpdate);
 	var deltaTime = (timeStamp - app.lastTime) / 1000;
 	app.lastTime = timeStamp;
-	// console.log(1/deltaTime);
+	// console.log(1/deltaTime); // view framerate in console
 
-//add if statement to optimize calling check wall & check paddle, only when necessary.
 	bounceWall();
 	bouncePaddle();
 
@@ -54,17 +58,24 @@ function drawScene(){
 }
 
 function bounceWall(){
-	if (app.ball.position.y >= (app.canvas.height - app.ball.radius) || 
-		app.ball.position.y <= app.ball.radius){
+// ball hits top or bottom wall -> bounce
+	if (app.ball.position.y >= (app.canvas.height - app.ball.radius)){
+		app.ball.position.y = (app.canvas.height - app.ball.radius - 1);
+		app.ball.speed.y = -(app.ball.speed.y);
+	}
+	if (app.ball.position.y <= app.ball.radius){
+		app.ball.position.y = (app.ball.radius + 1);
 		app.ball.speed.y = -(app.ball.speed.y);
 	}
 
+// ball hits right wall -> P2 scores, serve to P2
 	if (app.ball.position.x >= (app.canvas.width - app.ball.radius)){
 		app.playerTwo.score += 1;
 		updateScore();
 		spawnBall();
+		app.ball.speed.x = -(app.ball.speed.x);
 	}
-
+// ball hits left wall -> P1 scores, serve to P1
 	if (app.ball.position.x <= app.ball.radius){
 		app.playerOne.score += 1;
 		updateScore();
@@ -73,35 +84,92 @@ function bounceWall(){
 }
 
 function updateScore(){
-	document.getElementById('score').innerHTML = ('Score: ' + app.playerTwo.score + ' - ' + app.playerOne.score);
+	document.getElementById('score').innerHTML = (app.playerTwo.score + ' - ' + app.playerOne.score);
 }
 
 function bouncePaddle(){
 	let distFromP1Center = (app.ball.position.y - app.playerOne.position.y);
 	let distFromP2Center = (app.ball.position.y - app.playerTwo.position.y);
 
+// ball hits top half of P1 -> curve down
 	if(app.ball.position.x + app.ball.radius < app.playerOne.position.x + app.playerOne.size.width / 2 &&
 	   app.ball.position.x + app.ball.radius > app.playerOne.position.x - app.playerOne.size.width / 2 &&
 	   app.ball.position.y > app.playerOne.position.y - app.playerOne.size.height / 2 &&
-	   app.ball.position.y < app.playerOne.position.y + app.playerOne.size.height / 2)
+	   app.ball.position.y <= app.playerOne.position.y)
+	{
+		// increaseSpeed(app.ball.speed.x);
+		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
+		app.ball.speed.y = (distFromP1Center * 0.15);
+		app.ball.curve = 'down';
+	}
+// ball hits bottom half of P1 -> curve up
+	if(app.ball.position.x + app.ball.radius < app.playerOne.position.x + app.playerOne.size.width / 2 &&
+	   app.ball.position.x + app.ball.radius > app.playerOne.position.x - app.playerOne.size.width / 2 &&
+	   app.ball.position.y < app.playerOne.position.y + app.playerOne.size.height / 2 &&
+	   app.ball.position.y > app.playerOne.position.y)
 	{
 		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
 		app.ball.speed.y = (distFromP1Center * 0.15);
-		app.ball.curveUp = false;
-		app.ball.curveDown = true;
+		app.ball.curve = 'up';
 	}
-
+// ball hits top half of P2 -> curve down
 	if(app.ball.position.x - app.ball.radius > app.playerTwo.position.x - app.playerTwo.size.width / 2 &&
 	   app.ball.position.x - app.ball.radius < app.playerTwo.position.x + app.playerTwo.size.width / 2 &&
 	   app.ball.position.y > app.playerTwo.position.y - app.playerTwo.size.height / 2 &&
-	   app.ball.position.y < app.playerTwo.position.y + app.playerTwo.size.height / 2)
+	   app.ball.position.y < app.playerTwo.position.y)
 	{
 		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
 		app.ball.speed.y = (distFromP2Center * 0.15);
-		app.ball.curveDown = false;
-		app.ball.curveUp = true;
+		app.ball.curve = 'down';
+	}
+// ball hits bottom half of P2 -> curve up
+	if(app.ball.position.x - app.ball.radius > app.playerTwo.position.x - app.playerTwo.size.width / 2 &&
+	   app.ball.position.x - app.ball.radius < app.playerTwo.position.x + app.playerTwo.size.width / 2 &&
+	   app.ball.position.y < app.playerTwo.position.y + app.playerTwo.size.height / 2 &&
+	   app.ball.position.y >= app.playerTwo.position.y)
+	{
+		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
+		app.ball.speed.y = (distFromP2Center * 0.15);
+		app.ball.curve = 'up';
 	}
 }
+
+// function increaseSpeed(currentSpeed){
+// 	if (currentSpeed > 0 && Math.abs(currentSpeed) < app.ball.speed.max){
+// 		currentSpeed = -(currentSpeed + 0.4);
+// 	}
+// 	else if (currentSpeed < 0 && Math.abs(currentSpeed) < app.ball.speed.max){
+// 		currentSpeed = -(currentSpeed - 0.4);
+// 	}
+// 	else {
+// 		currentSpeed = -(currentSpeed);
+// 	}
+// }
+
+// function bouncePaddle(){
+// 	let distFromP1Center = (app.ball.position.y - app.playerOne.position.y);
+// 	let distFromP2Center = (app.ball.position.y - app.playerTwo.position.y);
+
+// 	if(app.ball.position.x + app.ball.radius < app.playerOne.position.x + app.playerOne.size.width / 2 &&
+// 	   app.ball.position.x + app.ball.radius > app.playerOne.position.x - app.playerOne.size.width / 2 &&
+// 	   app.ball.position.y > app.playerOne.position.y - app.playerOne.size.height / 2 &&
+// 	   app.ball.position.y < app.playerOne.position.y + app.playerOne.size.height / 2)
+// 	{
+// 		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
+// 		app.ball.speed.y = (distFromP1Center * 0.15);
+// 		app.ball.curve = 'down';
+// 	}
+
+// 	if(app.ball.position.x - app.ball.radius > app.playerTwo.position.x - app.playerTwo.size.width / 2 &&
+// 	   app.ball.position.x - app.ball.radius < app.playerTwo.position.x + app.playerTwo.size.width / 2 &&
+// 	   app.ball.position.y > app.playerTwo.position.y - app.playerTwo.size.height / 2 &&
+// 	   app.ball.position.y < app.playerTwo.position.y + app.playerTwo.size.height / 2)
+// 	{
+// 		app.ball.speed.x = -(app.ball.speed.x * app.ball.multiplier);
+// 		app.ball.speed.y = (distFromP2Center * 0.15);
+// 		app.ball.curve = 'up';
+// 	}
+// }
 
 function spawnBall(){
 	app.ball = {
@@ -110,22 +178,22 @@ function spawnBall(){
 			y: app.canvas.height / 2
 		},
 		speed: {
-			x: 4,
-			y: (Math.random() * 3) - 1
+			max: 9,
+			x: 7,
+			y: (Math.random() * 2) - 1
 		},
-		multiplier: 1.1,
+		curve: 'straight',
+		multiplier: 1,
 		radius: 5,
 		color: '#000000',
-		curveDown: false,
-		curveUp: false,
 		drawMe: function(context){
 			drawBall(context, this);
 		},
 		move : function(){
-			if(this.curveDown === true){
+			if(this.curve === 'down'){
 				this.speed.y += .1;
 			};
-			if(this.curveUp === true){
+			if(this.curve === 'up'){
 				this.speed.y -= .1;
 			}
 			this.position.x += this.speed.x;
@@ -143,17 +211,17 @@ function spawnPlayerOne(){
 		},
 		size: {
 			width: 8,
-			height: 100
+			height: 80
 		},
 		color: '#0000FF',
 		drawMe: function(context){
 			drawPaddle(context, this);
 		},
 		move : function(){
-			if(this.moveUp && this.position.y > 52){
+			if(this.moveUp && this.position.y > app.playerOne.size.height / 2){   //52
 				this.position.y -= 8;
 			}
-			if(this.moveDown && this.position.y < 352){
+			if(this.moveDown && this.position.y < app.canvas.height - (app.playerOne.size.height / 2) ){     //352
 				this.position.y += 8;
 			}
 		}
@@ -169,7 +237,7 @@ function spawnPlayerTwo(){
 		},
 		size: {
 			width: 8,
-			height: 100
+			height: 80
 		},
 		color: '#FF0000',
 		drawMe: function(context){
@@ -177,10 +245,10 @@ function spawnPlayerTwo(){
 		},
 		move : function(){
 			// this.position.y = app.ball.position.y; //for AI use.
-			if(this.moveUp && this.position.y > 52){
+			if(this.moveUp && this.position.y > app.playerTwo.size.height / 2){
 				this.position.y -= 8;
 			}
-			if(this.moveDown && this.position.y < 352){
+			if(this.moveDown && this.position.y < app.canvas.height - (app.playerTwo.size.height / 2)){
 				this.position.y += 8;
 			}
 		}
@@ -200,7 +268,6 @@ function drawBall(context, obj) {
     context.save();
     context.translate(obj.position.x, obj.position.y);
     context.fillStyle = obj.color;
-    // context.fillRect(obj.radius, obj.radius);
     context.stroke();
     context.beginPath();
     context.arc(0, 0, obj.radius, 0, (2 * Math.PI));
